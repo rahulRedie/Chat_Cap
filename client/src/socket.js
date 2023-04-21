a445ccfb4a71a3bd10f7b0e5991bb12895860991
rahulRedie <98146079+rahulRedie@users.noreply.github.com>
2023-04-21 07:25:50 -0500

    Update socket.js

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = "http://localhost:8080";
+const URL = "http://localhost:3001";
 const socket = io(URL, { autoConnect: false });
 
 export default socket;
